bba4709edf8c6d8573efdca3871e2bb6904a804c
Morswin <41349347+Morswin@users.noreply.github.com>
2025-06-08 14:13:03 +0200

    Nadgonić co miało być jeszcze na zajęciach

--- sklepNielogarytmiczny/src/main/java/pl/lamiglowki/sklepnielogarytmiczny/Cart.java
+++ sklepNielogarytmiczny/src/main/java/pl/lamiglowki/sklepnielogarytmiczny/Cart.java
@@ -10,6 +10,7 @@ import pl.lamiglowki.sklepnielogarytmiczny.repository.ItemRepository;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Component
@@ -19,53 +20,36 @@ public class Cart {
     private ArrayList<CartItem> cartItems = new ArrayList<CartItem>(){};
     private BigDecimal sum;
     private int counter = 0;
-//    public Cart(ItemRepository itemRepository) {
-//        this.itemRepository = itemRepository;
-//        List<Item> items = new ArrayList<>(itemRepository.findAll());
-//        for (Item item : items) {
-//            addItem(item);
-//            addItem(item);
-//        }
-//    }
+
     public void addItem(Item item) {
-//        System.out.println(cartItems);
-        if (!cartItems.isEmpty()) {
-            boolean found = false;
-            for (CartItem cartItem : cartItems) {
-                if (cartItem.isEquals(item)) {
-                    cartItem.increaseCounter();
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                cartItems.add(new CartItem(item));
-            }
-        }
-        else {
-            CartItem nowy = new CartItem(item);
-            cartItems.add(nowy);
-        }
+        Optional<CartItem> oItem = cartItems.stream()
+                .filter(ci -> ci.isEquals(item))
+                .findFirst();
+        oItem.ifPresentOrElse(
+                CartItem::increaseCounter,
+                () -> cartItems.add(new CartItem(item))
+        );
         recalculatePriceAndCounter();
     }
+
     public void removeItem(Item item) {
-        if (!cartItems.isEmpty()) {
-            for (CartItem cartItem : cartItems) {
-                if (cartItem.isEquals(item)) {
-                    cartItem.decreaseCounter();
-                    if (cartItem.getCounter() <= 0) {
-                        cartItems.remove(cartItem);
+        cartItems.stream()
+                .filter(ci -> ci.isEquals(item))
+                .findFirst()
+                .ifPresent(ci -> {
+                    ci.decreaseCounter();
+                    if (ci.getCounter() <- 0) {
+                        cartItems.remove(ci);
                     }
                     recalculatePriceAndCounter();
-                    break;
-                }
-            }
-        }
+                });
     }
+
     public void recalculatePriceAndCounter() {
         this.counter = this.cartItems.stream().mapToInt(CartItem::getCounter).sum();
         this.sum = this.cartItems.stream().map(CartItem::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
     public int getCounter() {
         return this.counter;
     }
