dd3064e1f041f7303abe3a77e53970f21dcf252c
Morswin <41349347+Morswin@users.noreply.github.com>
2025-06-15 21:03:11 +0200

    Zadanie domowe 2 z 4

--- PF/src/Main.java
+++ PF/src/Main.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        String[] friends = new String[] {"Piotr", "Alojzy", "Marysia"};
-        Arrays.sort(friends, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
-        System.out.println(Arrays.toString(friends));
-
-        ArrayList<String> enemies = new ArrayList<>(Arrays.asList(null, "Krzysztof", "Józef", null));
-        enemies.removeIf(e -> e == null);
-        System.out.println(enemies);
+        // Sortuję metodą instancji klasy
+        String[] names1 = new String[] { "Piotr", "Alojzy", "Marysia"};
+        Arrays.sort(names1, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names1));
+        // Usuwam nulle metodą statyczną klasy
+        ArrayList<String> names2 = new ArrayList<>(Arrays.asList(null, "Piotr", "Alojzy", "Marysia", null));
+        names2.removeIf(Objects::isNull);
+        System.out.println(names2);
+        // Wyświetlam metodą instancji obiektu
+        names2.forEach(System.out::println);
     }
 }
